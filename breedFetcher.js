38420d58c78f42295d85c7bd8103ee9db8878e80
vagrant <rah.leak@gmail.com>
2021-12-03 04:19:37 +0000

    cat breeds

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,10 +3,7 @@ const request = require('request');
 const fetchBreedDescription = function (breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     const data = JSON.parse(body);
-    if (error) {
-      console.log('error:', error.message);
-      return;
-    }
+    
     if (data.length === 0) {
       callback('no input found', null);
       return;
